e16589328fb24ed7ade3c896404010a89ea6edd0
gchecon <42677354+gchecon@users.noreply.github.com>
2024-02-23 10:44:28 -0300

    Função Renomear Arquivos Implantada

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config as cf
 import streamlit as st
+from datetime import datetime
 import os
 
 
@@ -13,6 +14,21 @@ def listar_arquivos(diretorio_inner):
     arquivos_inner = [f for f in os.listdir(diretorio_inner) if os.path.isfile(os.path.join(diretorio_inner, f))]
     return arquivos_inner
 
+def adicionar_prefixo_data_hora(diretorio, arquivos):
+    """Adiciona um prefixo com a data e hora de criação a cada arquivo no diretório."""
+    for arquivo in arquivos:
+        caminho_arquivo = os.path.join(diretorio, arquivo)
+        # Obter o timestamp de criação do arquivo e converter para data/hora
+        timestamp_criacao = os.path.getctime(caminho_arquivo)
+        data_hora_criacao = datetime.fromtimestamp(timestamp_criacao)
+        prefixo = data_hora_criacao.strftime('%Y%m%d_%H%M%S%f')[:-3] # Remove os últimos 3 dígitos para milissegundos
+
+        # Verificar se o arquivo já tem o prefixo
+        if not arquivo.startswith(prefixo):
+            novo_nome = f"{prefixo}_{arquivo}"
+            novo_caminho = os.path.join(diretorio, novo_nome)
+            os.rename(caminho_arquivo, novo_caminho)
+
 
 if __name__ == '__main__':
 
@@ -22,6 +38,14 @@ if __name__ == '__main__':
     st.set_page_config(page_title='Pendências SEI', layout='wide', page_icon=':page_with_curl:')
 
     option = st.sidebar.selectbox('Escolha uma opção:', subdiretorios)
+
+    if st.sidebar.button('Renomear Arquivos'):
+        dir_completo = os.path.join(cf.DIR_BASE, option)
+        arquivos = listar_arquivos(dir_completo)
+        adicionar_prefixo_data_hora(dir_completo, arquivos)
+        st.success("Arquivos renomeados com sucesso!")
+        arquivos_atualizados = listar_arquivos(dir_completo)
+
 
     # Lista todos os arquivos no diretório escolhido
     caminho_completo = os.path.join(cf.DIR_BASE, option)
